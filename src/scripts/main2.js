17c0520486296531c0228607c7e3767103dd3355
Miguel Luis <miguelluisataideferreira68@gmail.com>
2023-12-29 00:38:57 -0300

    add load component

--- src/scripts/main2.js
+++ src/scripts/main2.js
@@ -7,3 +7,17 @@ function openMenu(){
 function closeMenu(){
     ul.classList.remove('open');
 }
+
+const body = document.getElementById("main");
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Page has finished loading, hide the loader
+   body.style.display = "none"
+    document.querySelector('.loader-circle').style.display = 'block';
+});
+
+window.addEventListener("load", function () {
+    // Page has started loading, show the loader
+    document.querySelector('.loader-circle').style.display = 'none';
+    body.style.display = "block"
+});
